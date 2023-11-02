7a1a7d20c63cd3f70ef510b84e42289cf0d15948
mouhsin <boumargoud4mouhsin@gmail.com.com>
2023-11-02 21:35:38 +0100

    finiching ex2

---
+++ td/td3/ex2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+void triABull(list<int> *list){
+    int counter=0;
+    do{
+        for (){
+            if i
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int size,item;
+    list<int> Mylist;
+    cout << "combien des entier tu vas entre: ";
+    cin >> size;
+    for (int i=0 ; i<size; i++){
+        cout << "donnez le entier: ";
+        cin >> item;
+        Mylist.push_back(item);
+    }
+    for (const int item : Mylist){
+        cout << item << "  ";
+    }
+    return 0;
+}
