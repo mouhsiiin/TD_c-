889a7707810286f30e78b8ad4d6035ffc90ebe98
mouhsin <boumargoud4mouhsin@gmail.com.com>
2023-11-04 23:02:36 +0100

    finished

--- td/td3/ex5.cpp
+++ td/td3/ex5.cpp
@@ -4,24 +4,30 @@ class Test
 {
 public:
     static int tableau[];
-
-public:
-    static int division(int indice, int diviseur)
+    static float division(int indice, float diviseur)
     {
         return tableau[indice] / diviseur;
     }
 };
+
 int Test::tableau[] = {17, 12, 15, 38, 29, 157, 89, -22, 0, 5};
+
 int main()
 {
     int x, y;
-    cout << "Entrez l'indice de l'entier a diviser: " << endl;
+    cout << "Entrez l'indice de l'entier a diviser: " ;
     cin >> x;
-    cout << "Entrez le diviseur: " << endl;
+    cout << "Entrez le diviseur: " ;
     cin >> y;
     cout << "Le resultat de la division est: " << endl;
-    try:
+    try{
+
         cout << Test::division(x, y) << endl;
-    e
+    }
+    catch (exception){
+        
+        cout << "division par zero "<< endl;
+        exit(-2);
+    }
     return 0;
 }
